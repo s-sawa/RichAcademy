a9de0c13c100ece1f674737d493397c79a0db221
s-sawa <s.s.hrmeee21@gmail.com>
2023-04-19 18:49:45 +0900

    バックアップ

--- js/main.js
+++ js/main.js
@@ -1,3 +1,17 @@
 function onClickAlert() {
-    alert("未実装！");
-  }
+  alert("未実装！");
+}
+$(function () {
+  // ハンバーガーメニューのクリックイベント
+  $(".hamburger").click(function () {
+    $(".nav").slideToggle();
+  });
+
+  // リサイズイベント
+  $(window).resize(function () {
+    var w = $(window).width();
+    if (w > 768) {
+      $(".nav").removeAttr("style");
+    }
+  });
+});
